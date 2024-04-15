e4f2cbd856c5114cb3ee781f89254c0d9bdd991c
Alice Jacka <jacka.alice@gmail.com>
2024-04-15 11:35:45 +1000

    Add intital version

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# WheresTheFrontEnd
+# Where's The Front End?!
+
+Front end for the Where's The project.
+
+## Sources 
+
+- [Transport mode pictograms](https://discover.data.vic.gov.au/dataset/transport-mode-pictograms)
+
+## License
+
+    Where's The Tram
+    Copyright (C) 2024  Alice Jacka
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published
+    by the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
---
+++ src/main.js
@@ -0,0 +1,47 @@
+const map = L.map('map').setView([-37.810175, 144.970194], 13);
+
+const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  maxZoom: 19,
+  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+const tramIcon = L.icon({
+  iconUrl: 'https://vicroadsopendatastorehouse.vicroads.vic.gov.au/opendata/Public_Transport/Pictograms/PICTO-MODE-Tram.svg',
+  iconSize: [30, 30],
+  iconAnchor: [15, 15],
+  popupAnchor: [15, 0],
+});
+
+var trams = [];
+update();
+setInterval(() => {
+  update();
+}, 15000);
+
+function update() {
+  fetch('/api', {
+    method: 'GET',
+    // Request headers
+    headers: {
+      'Cache-Control': 'no-cache',
+      'Ocp-Apim-Subscription-Key': 'API KEY'
+    }
+  }).then((response) => {
+    if (response.status !== 200) {
+      console.log('Error fetching data'); // TODO: visual error message
+      return;
+    }
+
+    response.json().then((data) => {
+      for (let i = 0; i < trams.length; i++) {
+        trams[i].remove();
+      }
+      trams = [];
+
+      for (let i = 0; i < data.entity.length; i++) {
+        const position = data.entity[i].vehicle.position;
+        L.marker([position.latitude, position.longitude], {icon: tramIcon}).addTo(map);
+      }
+    });
+  });
+}
